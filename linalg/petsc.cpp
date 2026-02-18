2e8dd69e29b9e5f86f332383c82628534f92bafc
Satish Balay <balay@mcs.anl.gov>
2026-02-18 12:10:17 -0600

    update PetscCtxDestroyFn usage for < (3,23,0)

--- linalg/petsc.cpp
+++ linalg/petsc.cpp
@@ -41,6 +41,9 @@
 #if PETSC_VERSION_LT(3,23,0)
 #define PetscContainerSetCtxDestroy(A,B) PetscContainerSetUserDestroy(A,B)
 #endif
+#if PETSC_VERSION_LT(3,23,0)
+typedef PetscErrorCode (PetscCtxDestroyFn)(void**);
+#endif
 
 #include <fstream>
 #include <iomanip>
@@ -2472,7 +2475,6 @@ void PetscSolver::SetMaxIter(int max_iter)
 
 void PetscSolver::SetPrintLevel(int plev)
 {
-   typedef PetscErrorCode (*myPetscFunc)(void**);
    PetscViewerAndFormat *vf = NULL;
    PetscViewer viewer = PETSC_VIEWER_STDOUT_(PetscObjectComm(obj));
 
@@ -2498,14 +2500,14 @@ void PetscSolver::SetPrintLevel(int plev)
 #else
          ierr = KSPMonitorSet(ksp,(myMonitor)KSPMonitorResidual,vf,
 #endif
-                              (myPetscFunc)PetscViewerAndFormatDestroy);
+                              (PetscCtxDestroyFn *)PetscViewerAndFormatDestroy);
          PCHKERRQ(ksp,ierr);
       }
       else if (plev > 1)
       {
          ierr = KSPSetComputeSingularValues(ksp,PETSC_TRUE); PCHKERRQ(ksp,ierr);
          ierr = KSPMonitorSet(ksp,(myMonitor)KSPMonitorSingularValue,vf,
-                              (myPetscFunc)PetscViewerAndFormatDestroy);
+                              (PetscCtxDestroyFn *)PetscViewerAndFormatDestroy);
          PCHKERRQ(ksp,ierr);
          if (plev > 2)
          {
@@ -2516,7 +2518,7 @@ void PetscSolver::SetPrintLevel(int plev)
 #else
             ierr = KSPMonitorSet(ksp,(myMonitor)KSPMonitorTrueResidual,vf,
 #endif
-                                 (myPetscFunc)PetscViewerAndFormatDestroy);
+                                 (PetscCtxDestroyFn *)PetscViewerAndFormatDestroy);
             PCHKERRQ(ksp,ierr);
          }
       }
@@ -2532,7 +2534,7 @@ void PetscSolver::SetPrintLevel(int plev)
       if (plev > 0)
       {
          ierr = SNESMonitorSet(snes,(myMonitor)SNESMonitorDefault,vf,
-                               (myPetscFunc)PetscViewerAndFormatDestroy);
+                               (PetscCtxDestroyFn *)PetscViewerAndFormatDestroy);
          PCHKERRQ(snes,ierr);
       }
    }
